11847e547dd7866bf79ac2be304dc7bd58cd71d6
adarsh1722 <adarsh0004singh@gmail.com>
2021-06-25 20:17:54 +0530

    drum files added

---
+++ index.js
@@ -0,0 +1,84 @@
+
+var n = document.querySelectorAll(".drum").length;
+// for click
+for(var i = 0 ; i < n ; i ++) {
+   
+    document.querySelectorAll("button")[i].addEventListener("click" , function () {
+       
+       var buttonInnerHTML = this.innerHTML;
+       makesound(buttonInnerHTML);
+       buttonAnimation(buttonInnerHTML);
+       
+    });
+
+};
+
+// for button press
+document.addEventListener("keypress" , function(event) {
+
+    makesound(event.key);
+    buttonAnimation(event.key);
+
+} );
+
+// function 
+    function makesound(key){
+          
+        switch(key) {
+          
+            case "w" :
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break; 
+           
+            case "a" :
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break; 
+            case "s" :
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case "d" :
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break; 
+            case "j" :
+                var tom5 = new Audio("sounds/snare.mp3");
+                tom5.play();
+                break; 
+           case "k" :
+                var tom6 = new Audio("sounds/crash.mp3");
+                tom6.play();
+                break; 
+           case "l" :
+                var tom7 = new Audio("sounds/kick-bass.mp3");
+                tom7.play();
+                break; 
+    
+            default : console.log("buttonInnerHTML");
+        
+    
+        }
+    
+
+
+
+    }
+
+function buttonAnimation(currentkey){
+
+    var activeButton = document.querySelector("."+ currentkey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    },100);
+
+
+}
+     
+
+
+
+
